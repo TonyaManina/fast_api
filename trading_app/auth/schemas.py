44151069eb34045e222801c26e75540445fd360e
Manina Antonina <tonya.manina@gmail.com>
2024-02-24 00:10:55 +0700

    jopa

---
+++ trading_app/auth/schemas.py
@@ -0,0 +1,20 @@
+from fastapi_users import schemas
+from typing import Optional
+
+class UserRead(schemas.BaseUser[int]):
+    id: int
+    email: str
+    username: str
+    role_id: int
+    is_active: bool = True
+    is_superuser: bool = False
+    is_verified: bool = False
+
+class UserCreate(schemas.BaseUserCreate):
+    username: str
+    role_id: int
+    email: str
+    password: str
+    is_active: Optional[bool] = True
+    is_superuser: Optional[bool] = False
+    is_verified: Optional[bool] = False
